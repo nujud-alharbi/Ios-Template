bffcd06b6550825cb4d264f131c131b04cc6a3d9
nujudalalawi <njoud.aali.med.s@tuwaiq.edu.sa>
2023-05-02 10:08:04 -0700

    first commit

---
+++ template-ios/localizationView.swift
@@ -0,0 +1,20 @@
+//
+//  localizationView.swift
+//  template-ios
+//
+//  Created by Nujud Alalawi on 11/10/1444 AH.
+//
+
+import UIKit
+
+class localizationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
