8a5be445aa004d5e12600f17e3e406b3f7b313e9
hananasiri <kayan2222762YAHOO.COM>
2023-05-07 13:36:16 +0300

    end: theme

---
+++ template-ios/core/theme/themeProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  themeProtocol.swift
+//  template-ios
+//
+//  Created by Hanan Asiri on 17/10/1444 AH.
+//
+
+import UIKit
+
+protocol ThemeProtocol {
+    var accent : UIColor {get}
+    var background : UIColor {get}
+    var tint : UIColor {get}
+    var iconesun : UIColor {get}
+    
+
+}
